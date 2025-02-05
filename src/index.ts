94ef43dd6856c3b0d39e15dc5f4d0ce7644e22ce
ramonrash2 <ramonrash2@gmail.com>
2025-02-05 12:44:44 +0000

    added queue

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,9 @@ import { zValidator } from "@hono/zod-validator";
 import { Hono } from "hono";
 import { z } from "zod";
 import { queue, worker } from "./queue.js";
+import type { Job } from "bullmq";
+import { farmer_db } from "./db/index.js";
+import { farmersTable } from "./db/schema.js";
 
 const app = new Hono();
 
@@ -10,16 +13,13 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-// app.get("/create-farmer", async (c) => {
-//   await farmer_db.insert(farmersTable).values({
-//     name: "John Doe Farmer",
-//     age: 25,
-//     email: "johndoe@exmaple.com",
-//   });
-//   return c.json({ message: "Farmer Created" });
-// });
 
-app.get(
+app.get("/farmers", async (c) => {
+  const farmers = await farmer_db.select().from(farmersTable)
+  return c.json(farmers)
+})
+
+app.post(
   "/data/upload",
   zValidator(
     "json",
@@ -42,6 +42,24 @@ console.log(`Server is running on http://localhost:${port}`);
 worker.on("ready", () => {
   // eslint-disable-next-line no-console
   console.log("Worker is ready and listening for jobs!");
+});
+worker.on("completed", (job: Job) => {
+  console.log(`Job ${job.id} completed, task name: ${job.name}`);
+});
+
+worker.on("failed", (job: Job | undefined, error: Error) => {
+  if (job) {
+    console.log(
+      `Job ${job.id} failed, task name: ${job.name}, error: ${error.message}`,
+    );
+  }
+  else {
+    console.log(`Job failed, error: ${error.message}`);
+  }
+});
+
+worker.on("error", (err) => {
+  console.log(err);
 });
 
 serve({
